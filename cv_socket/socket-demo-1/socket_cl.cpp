82b4cbb2e8b7c871ab338c7b77b2ff28972745de
Dávid Jopčík <david.jopcik@gmail.com>
2025-11-09 07:47:51 +0100

    DU3-3 final

--- cv_socket/socket-demo-1/socket_cl.cpp
+++ cv_socket/socket-demo-1/socket_cl.cpp
@@ -29,42 +29,43 @@
 #include <errno.h>
 #include <netdb.h>
 
-#define STR_CLOSE               "close"
+#define STR_CLOSE "close"
 
 //***************************************************************************
 // log messages
 
-#define LOG_ERROR               0       // errors
-#define LOG_INFO                1       // information and notifications
-#define LOG_DEBUG               2       // debug messages
+#define LOG_ERROR 0 // errors
+#define LOG_INFO 1  // information and notifications
+#define LOG_DEBUG 2 // debug messages
 
 // debug flag
 int g_debug = LOG_INFO;
 
-void log_msg( int t_log_level, const char *t_form, ... )
+void log_msg(int t_log_level, const char *t_form, ...)
 {
     const char *out_fmt[] = {
-            "ERR: (%d-%s) %s\n",
-            "INF: %s\n",
-            "DEB: %s\n" };
+        "ERR: (%d-%s) %s\n",
+        "INF: %s\n",
+        "DEB: %s\n"};
 
-    if ( t_log_level && t_log_level > g_debug ) return;
+    if (t_log_level && t_log_level > g_debug)
+        return;
 
-    char l_buf[ 1024 ];
+    char l_buf[1024];
     va_list l_arg;
-    va_start( l_arg, t_form );
-    vsprintf( l_buf, t_form, l_arg );
-    va_end( l_arg );
+    va_start(l_arg, t_form);
+    vsprintf(l_buf, t_form, l_arg);
+    va_end(l_arg);
 
-    switch ( t_log_level )
+    switch (t_log_level)
     {
     case LOG_INFO:
     case LOG_DEBUG:
-        fprintf( stdout, out_fmt[ t_log_level ], l_buf );
+        fprintf(stdout, out_fmt[t_log_level], l_buf);
         break;
 
     case LOG_ERROR:
-        fprintf( stderr, out_fmt[ t_log_level ], errno, strerror( errno ), l_buf );
+        fprintf(stderr, out_fmt[t_log_level], errno, strerror(errno), l_buf);
         break;
     }
 }
@@ -72,9 +73,9 @@ void log_msg( int t_log_level, const char *t_form, ... )
 //***************************************************************************
 // help
 
-void help( int t_narg, char **t_args )
+void help(int t_narg, char **t_args)
 {
-    if ( t_narg <= 1 || !strcmp( t_args[ 1 ], "-h" ) )
+    if (t_narg <= 1 || !strcmp(t_args[1], "-h"))
     {
         printf(
             "\n"
@@ -84,227 +85,132 @@ void help( int t_narg, char **t_args )
             "\n"
             "    -d  debug mode \n"
             "    -h  this help\n"
-            "\n", t_args[ 0 ] );
+            "\n",
+            t_args[0]);
 
-        exit( 0 );
+        exit(0);
     }
 
-    if ( !strcmp( t_args[ 1 ], "-d" ) )
+    if (!strcmp(t_args[1], "-d"))
         g_debug = LOG_DEBUG;
 }
 
 //***************************************************************************
 
-int main( int t_narg, char **t_args )
+int main(int t_narg, char **t_args)
 {
 
-    if ( t_narg <= 2 ) help( t_narg, t_args );
+    if (t_narg <= 2)
+        help(t_narg, t_args);
 
     int l_port = 0;
     char *l_host = nullptr;
     char *l_resolution = nullptr;
 
-
     // parsing arguments
-    for ( int i = 1; i < t_narg; i++ )
+    for (int i = 1; i < t_narg; i++)
     {
-        if ( !strcmp( t_args[ i ], "-d" ) )
+        if (!strcmp(t_args[i], "-d"))
             g_debug = LOG_DEBUG;
 
-        if ( !strcmp( t_args[ i ], "-h" ) )
-            help( t_narg, t_args );
+        if (!strcmp(t_args[i], "-h"))
+            help(t_narg, t_args);
 
-        if ( *t_args[ i ] != '-' )
+        if (*t_args[i] != '-')
         {
-            if ( !l_host )
-                l_host = t_args[ i ];
-            else if ( !l_port )
-                l_port = atoi( t_args[ i ] );
-            else if ( !l_resolution )
-                l_resolution = t_args[ i ];
+            if (!l_host)
+                l_host = t_args[i];
+            else if (!l_port)
+                l_port = atoi(t_args[i]);
+            else if (!l_resolution)
+                l_resolution = t_args[i];
         }
-
     }
 
-    if ( !l_host || !l_port )
+    if (!l_host || !l_port)
     {
-        log_msg( LOG_INFO, "Host or port is missing!" );
-        help( t_narg, t_args );
-        exit( 1 );
+        log_msg(LOG_INFO, "Host or port is missing!");
+        help(t_narg, t_args);
+        exit(1);
     }
 
-    log_msg( LOG_INFO, "Connection to '%s':%d. Resolution is: %s\n", l_host, l_port, l_resolution );
+    log_msg(LOG_INFO, "Connection to '%s':%d. Resolution is: %s\n", l_host, l_port, l_resolution);
 
     addrinfo l_ai_req, *l_ai_ans;
-    bzero( &l_ai_req, sizeof( l_ai_req ) );
+    bzero(&l_ai_req, sizeof(l_ai_req));
     l_ai_req.ai_family = AF_INET;
     l_ai_req.ai_socktype = SOCK_STREAM;
 
-    int l_get_ai = getaddrinfo( l_host, nullptr, &l_ai_req, &l_ai_ans );
-    if ( l_get_ai )
+    int l_get_ai = getaddrinfo(l_host, nullptr, &l_ai_req, &l_ai_ans);
+    if (l_get_ai)
     {
-        log_msg( LOG_ERROR, "Unknown host name!" );
-        exit( 1 );
+        log_msg(LOG_ERROR, "Unknown host name!");
+        exit(1);
     }
 
-    sockaddr_in l_cl_addr =  *( sockaddr_in * ) l_ai_ans->ai_addr;
-    l_cl_addr.sin_port = htons( l_port );
-    freeaddrinfo( l_ai_ans );
+    sockaddr_in l_cl_addr = *(sockaddr_in *)l_ai_ans->ai_addr;
+    l_cl_addr.sin_port = htons(l_port);
+    freeaddrinfo(l_ai_ans);
 
     // socket creation
-    int l_sock_server = socket( AF_INET, SOCK_STREAM, 0 );
-    if ( l_sock_server == -1 )
+    int l_sock_server = socket(AF_INET, SOCK_STREAM, 0);
+    if (l_sock_server == -1)
     {
-        log_msg( LOG_ERROR, "Unable to create socket.");
-        exit( 1 );
+        log_msg(LOG_ERROR, "Unable to create socket.");
+        exit(1);
     }
 
     // connect to server
-    if ( connect( l_sock_server, ( sockaddr * ) &l_cl_addr, sizeof( l_cl_addr ) ) < 0 )
+    if (connect(l_sock_server, (sockaddr *)&l_cl_addr, sizeof(l_cl_addr)) < 0)
     {
-        log_msg( LOG_ERROR, "Unable to connect server." );
-        exit( 1 );
+        log_msg(LOG_ERROR, "Unable to connect server.");
+        exit(1);
     }
 
-    uint l_lsa = sizeof( l_cl_addr );
+    uint l_lsa = sizeof(l_cl_addr);
     // my IP
-    getsockname( l_sock_server, ( sockaddr * ) &l_cl_addr, &l_lsa );
-    log_msg( LOG_INFO, "My IP: '%s'  port: %d",
-             inet_ntoa( l_cl_addr.sin_addr ), ntohs( l_cl_addr.sin_port ) );
+    getsockname(l_sock_server, (sockaddr *)&l_cl_addr, &l_lsa);
+    log_msg(LOG_INFO, "My IP: '%s'  port: %d",
+            inet_ntoa(l_cl_addr.sin_addr), ntohs(l_cl_addr.sin_port));
     // server IP
-    getpeername( l_sock_server, ( sockaddr * ) &l_cl_addr, &l_lsa );
-    log_msg( LOG_INFO, "Server IP: '%s'  port: %d",
-             inet_ntoa( l_cl_addr.sin_addr ), ntohs( l_cl_addr.sin_port ) );
+    getpeername(l_sock_server, (sockaddr *)&l_cl_addr, &l_lsa);
+    log_msg(LOG_INFO, "Server IP: '%s'  port: %d",
+            inet_ntoa(l_cl_addr.sin_addr), ntohs(l_cl_addr.sin_port));
 
-    log_msg( LOG_INFO, "Enter 'close' to close application." );
-
-
-   /*  char l_buf[ 128 ];
-    snprintf(l_buf, sizeof(l_buf), "%s\n", l_resolution);
-    int l_len = write( l_sock_server, l_buf, sizeof( l_buf) );
-
-    int out_fd = open("image.img", O_CREAT | O_TRUNC | O_WRONLY, 0644); */
+    log_msg(LOG_INFO, "Enter 'close' to close application.");
 
     char line[128];
-int n = snprintf(line, sizeof(line), "%s\n", l_resolution);
-if (n <= 0 || n >= (int)sizeof(line)) { perror("bad resolution"); return 1; }
+    int n = snprintf(line, sizeof(line), "%s\n", l_resolution);
 
-// spoľahlivý zápis n bajtov (ošetrenie partial write a EINTR)
-for (int off = 0; off < n; ) {
-    ssize_t w = write(l_sock_server, line + off, n - off);
-    if (w < 0) { if (errno == EINTR) continue; perror("write"); return 1; }
-    off += (int)w;
-}
-
-    /* char i_buf[128];
-    while (1)
+    //write data
+    for (int off = 0; off < n;)
     {
-        int r = read(l_sock_server, i_buf, sizeof(i_buf));
-        if (r == 0)
-        {
-            break;
-        }
-        ssize_t out = write(out_fd, i_buf, sizeof(i_buf));
-    } */
+        ssize_t w = write(l_sock_server, line + off, n - off);
+        off += (int)w;
+    } 
 
-    //ssize_t out = write(out_fd, i_buf, sizeof(i_buf));
 
     int out_fd = open("image.img", O_CREAT | O_TRUNC | O_WRONLY, 0644);
-if (out_fd < 0) { perror("open image.img"); return 1; }
 
-char buf[4096];
-for (;;) {
-    ssize_t r = read(l_sock_server, buf, sizeof(buf));
-    if (r < 0) {
-        if (errno == EINTR) continue;
-        perror("read");
-        close(out_fd);
-        return 1;
-    }
-    if (r == 0) break; 
-
-    ssize_t off = 0;
-    while (off < r) {
-        ssize_t w = write(out_fd, buf + off, r - off);
-        if (w < 0) { if (errno == EINTR) continue; perror("write file"); close(out_fd); return 1; }
-        off += w;
-    }
-}
-close(out_fd);
-
-    
-
-
-
-
-    // list of fd sources
-    /* pollfd l_read_poll[ 2 ];
-
-    l_read_poll[ 0 ].fd = STDIN_FILENO;
-    l_read_poll[ 0 ].events = POLLIN;
-    l_read_poll[ 1 ].fd = l_sock_server;
-    l_read_poll[ 1 ].events = POLLIN;
-
-    // go!
-    while ( 1 )
+    char buf[4096];
+    while (1)
     {
-        char l_buf[ 128 ];
+        ssize_t r = read(l_sock_server, buf, sizeof(buf));
+        if (r == 0)
+            break;
 
-        // select from fds
-        if ( poll( l_read_poll, 2, -1 ) < 0 ) break;
-
-        // data on stdin?
-        if ( l_read_poll[ 0 ].revents & POLLIN )
+        ssize_t off = 0;
+        while (off < r)
         {
-            //  read from stdin
-            int l_len = read( STDIN_FILENO, l_resolution, sizeof( l_buf ) );
-            if ( l_len < 0 )
-                log_msg( LOG_ERROR, "Unable to read from stdin." );
-            else
-                log_msg( LOG_DEBUG, "Read %d bytes from stdin.", l_len );
-
-            // send data to server
-            l_len = write( l_sock_server, l_resolution, l_len );
-            if ( l_len < 0 )
-                log_msg( LOG_ERROR, "Unable to send data to server." );
-            else
-                log_msg( LOG_DEBUG, "Sent %d bytes to server.", l_len );
+            ssize_t w = write(out_fd, buf + off, r - off);
+            off += w;
         }
+    }
+    close(out_fd);
 
-        // data from server?
-        if ( l_read_poll[ 1 ].revents & POLLIN )
-        {
-            // read data from server
-            int l_len = read( l_sock_server, l_buf, sizeof( l_buf ) );
-            if ( !l_len )
-            {
-                log_msg( LOG_DEBUG, "Server closed socket." );
-                break;
-            }
-            else if ( l_len < 0 )
-            {
-                log_msg( LOG_ERROR, "Unable to read data from server." );
-                break;
-            }
-            else
-                log_msg( LOG_DEBUG, "Read %d bytes from server.", l_len );
-
-            // display on stdout
-            l_len = write( STDOUT_FILENO, l_resolution, l_len );
-            if ( l_len < 0 )
-                log_msg( LOG_ERROR, "Unable to write to stdout." );
-
-            // request to close?
-            if ( !strncasecmp( l_buf, STR_CLOSE, strlen( STR_CLOSE ) ) )
-            {
-                log_msg( LOG_INFO, "Connection will be closed..." );
-                break;
-            }
-        }
-    } */
 
     // close socket
-    close( l_sock_server );
+    close(l_sock_server);
 
     return 0;
-  }
+}
