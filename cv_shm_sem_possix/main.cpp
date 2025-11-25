1e419d584dabf3b3a7216ad2cd98e969b7389f6f
Dávid Jopčík <david.jopcik@gmail.com>
2025-11-25 19:33:30 +0100

    shm open

--- cv_shm_sem_possix/main.cpp
+++ cv_shm_sem_possix/main.cpp
@@ -7,7 +7,7 @@
 #include <sys/stat.h>   // mode_t, 0666
 #include <sys/mman.h>   // shm_open, mmap, PROT_*, MAP_*
 #include <semaphore.h>  // sem_open, sem_wait, sem_post
-//#include <mqueue.h>     // mq_open, mq_send, mq_receive
+#include <mqueue.h>     // mq_open, mq_send, mq_receive
 #include <sys/wait.h>   // wait
 
 typedef struct {
@@ -26,6 +26,17 @@ void sem_open_test(){
     printf("Som v kritickej sekcii...\n");
     sleep(3);
     printf("Opustam kriticku sekciu...\n");
+}
+
+void demo_shm_open_mmap() {
+    const char *SHM_NAME = "/demo_shm_open";
+
+    int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    size_t size = sizeof(shm_buffer_t);
+    ftruncate(fd, size);
+
+    shm_buffer_t *buf = (shm_buffer_t *)mmap(NULL, size, PROT_READ |PROT_WRITE, MAP_SHARED, fd, 0);
+    
 }
 
 
